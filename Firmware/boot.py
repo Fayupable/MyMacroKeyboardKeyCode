4fa782c771e6d9c90aecb27c4708ff37dd7ef5bc
Fayupable <90789180+Fayupable@users.noreply.github.com>
2023-08-07 12:38:46 +0300

    Add files via upload

---
+++ Firmware/boot.py
@@ -0,0 +1,18 @@
+import supervisor
+import board
+import storage
+from digitalio import DigitalInOut, Direction, Pull
+
+supervisor.set_next_stack_limit(4096 + 4096)
+
+row = DigitalInOut(board.D7)
+col = DigitalInOut(board.D6)
+
+row.direction = Direction.INPUT
+col.direction = Direction.OUTPUT
+
+row.pull = Pull.DOWN
+col.value = True
+
+if not row.value:
+    storage.disable_usb_drive()
